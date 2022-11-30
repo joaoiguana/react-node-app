f2e649305451dfc226d7669a39bfddb5cf88c302
joao garcia <jiboiadamor@gmail.com>
2022-11-30 11:23:05 +0000

    server running on PORT 3001 with get action for API

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening o PORT ${PORT}...`)
 })
